a392641f18fd4756fef295a80e53de61d0e98896
Gabriel <gabrielmaa@yahoo.com.br>
2021-03-30 18:14:53 -0300

    Lab3 Exercício 3 pronto

---
+++ Lab3_exerc03/Lab3_exerc03/JogoDigital.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_exerc03
+{
+    class JogoDigital : Produto
+    {
+        public string Plataforma { get; private set; }
+
+        public JogoDigital(string titulo, double preco, string plataforma) : base(titulo, preco)
+        {
+            Plataforma = plataforma;
+        }
+
+        public override void InformarDescricao()
+        {
+            Console.WriteLine($"{Titulo} para {Plataforma}");
+        }
+    }
+}
---
+++ Lab3_exerc03/Lab3_exerc03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_exerc03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto jogo1 = new JogoDigital("FIFA 2021", 350, "PS5");
+            Produto jogo2 = new JogoDigital("Persona 5 Strikers", 300, "PS5");
+            Produto livro1 = new Livro("Harry Potter", 40, "J. K. Rowling");
+            Produto livro2 = new Livro("Game of Thrones", 50, "George R. R. Martin");
+
+            Produto[] produtos = { jogo1, jogo2, livro1, livro2 };
+
+            foreach (Produto item in produtos)
+            {
+                item.InformarDescricao();
+                item.InformarPreco();
+                Console.WriteLine("------------------------------");
+                Console.ReadLine();
+            }
+        }
+    }
+}
